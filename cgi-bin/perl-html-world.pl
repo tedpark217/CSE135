c552a8efa77e3d369fcff85d427d1bcb731599ce
tedpark217 <jhp006@ucsd.edu>
2023-04-22 03:48:53 +0000

    u

--- cgi-bin/perl-html-world.pl
+++ cgi-bin/perl-html-world.pl
@@ -8,7 +8,7 @@ print "<title>TEAM TDH - Hello, Perl!</title>";
 print "</head>";
 print "<body>";
 
-print "<h1>TEAM TDH -Hello, Perl!</h1>";
+print "<h1>TEAM TDH - Hello, Perl!</h1>";
 print "<p>This page was generated with the Perl programming langauge</p>";
 
 $date = localtime();
