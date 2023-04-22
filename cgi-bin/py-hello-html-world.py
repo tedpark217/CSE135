f5d7c44adc90e5762f86a63b3fda8a149d50a896
tedpark217 <jhp006@ucsd.edu>
2023-04-22 14:52:19 -0700

    u

---
+++ cgi-bin/py-hello-html-world.py
@@ -0,0 +1,12 @@
+print("Cache-Control: no-cache\n");
+print("Content-type: text/html\n\n");
+
+print("<html><head><title>TEAM TDH - Hello CGI World</title></head>\
+	<body><h1 align=center>TEAM TDH - Hello HTML World</h1>\
+  	<hr/>\n");
+
+print("TEAM TDH - Hello World<br/>\n");
+#print("This program was generated at: %s\n<br/>", ctime(&t));
+#print("Your current IP address is: %s<br/>", getenv("REMOTE_ADDR"));
+
+print("</body></html>");
