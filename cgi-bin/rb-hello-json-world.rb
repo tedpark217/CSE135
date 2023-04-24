1313b17784e4b1746a63feb251674c90d6d6b903
tedpark217 <jhp006@ucsd.edu>
2023-04-24 00:59:34 +0000

    u

--- cgi-bin/rb-hello-json-world.rb
+++ cgi-bin/rb-hello-json-world.rb
@@ -16,7 +16,7 @@ puts "</head>"
 
 puts "<body>" 
 
-arr = {:title => "TEAM TDH - Hello, Ruby!", :heading => "TEAM TDH - Hello, Ruby!", :message => "This page was generated with the PHP programming language", :time => time.ctime, :IP => ENV['REMOTE_ADDR']} 
+arr = {:title => "TEAM TDH - Hello, Ruby!", :heading => "TEAM TDH - Hello, Ruby!", :message => "This page was generated with the Ruby programming language", :time => time.ctime, :IP => ENV['REMOTE_ADDR']} 
 
 puts JSON.pretty_generate(arr, :indent => "\t")
 
